6df5b74a198cc7b01c7dd8d577124f411a4e6f4e
RaviDeveloper1 <aravi.us80@gmail.com>
2018-03-04 12:46:46 -0600

    removed BPanther, add Avengers

--- src/app/marvelheros.service.ts
+++ src/app/marvelheros.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MarvelherosService {
-  heros = ['Iron Man', 'Spider Man', 'Black Panther'];
+  heros = ['Iron Man', 'Spider Man', 'Avengers'];
 
   constructor() { }
 
